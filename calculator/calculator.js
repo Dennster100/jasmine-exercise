3d9c0ad9847859caafdfbc4af9992fb0f1f568dc
Dennis Taranchuk <dennistaranchuk@outlook.com>
2024-04-03 11:21:45 -0700

    added all the files

---
+++ calculator/calculator.js
@@ -0,0 +1,50 @@
+window.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById("calc-form");
+  if (form) {
+    setupIntialValues();
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      update();
+    });
+  }
+});
+
+function getCurrentUIValues() {
+  return {
+    amount: +(document.getElementById("loan-amount").value),
+    years: +(document.getElementById("loan-years").value),
+    rate: +(document.getElementById("loan-rate").value),
+  }
+}
+
+// Get the inputs from the DOM.
+// Put some default values in the inputs
+// Call a function to calculate the current monthly payment
+function setupIntialValues() {
+
+}
+
+// Get the current values from the UI
+// Update the monthly payment
+function update() {
+  
+}
+
+// Given an object of values (a value has amount, years and rate ),
+// calculate the monthly payment.  The output should be a string
+// that always has 2 decimal places.
+function calculateMonthlyPayment(values) {
+  let i = values.rate * 12;
+  let P = values.amount;
+  let n = values.years * 12;
+  let monthlyRate = (i * P) / (1 - Math.pow((1 + i), -n));
+
+  return monthlyRate;
+
+}
+
+// Given a string representing the monthly payment value,
+// update the UI to show the value.
+function updateMonthly(monthly) {
+
+}
